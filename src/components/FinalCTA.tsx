273e4ac0a05fe8f70da273297ea9bd71fbaa6e1f
puralumina <puraluminaofficial@gmail.com>
2025-07-18 19:49:48 -0400

    Updated FinalCTA.tsx

--- src/components/FinalCTA.tsx
+++ src/components/FinalCTA.tsx
@@ -61,7 +61,7 @@ const FinalCTA = () => {
           </div><br></br><br></br>
 
           {/* Pricing */}
-          <div className="bg-white bg-opacity-20 rounded-2xl p-8 mb-8">
+          <div className="bg-green bg-opacity-20 rounded-2xl p-8 mb-8">
             <div className="flex items-center justify-center mb-6">
               <Clock className="h-6 w-6 mr-2 text-yellow-400" />
               <span className="text-lg font-semibold">Offer expires in 48 hours</span>
