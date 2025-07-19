fd386b52bcff07f48e59b9033760c077878e38d5
puralumina <puraluminaofficial@gmail.com>
2025-07-18 20:16:32 -0400

    Updated FinalCTA.tsx

--- src/components/FinalCTA.tsx
+++ src/components/FinalCTA.tsx
@@ -92,7 +92,7 @@ const FinalCTA = () => {
             </div>
 
             <div className="bg-green-500 text-white px-4 py-2 rounded-lg inline-block font-semibold">
-              Save $350 Today (88% OFF)
+              Save $350 Today (87% OFF)
             </div>
           </div>
 
